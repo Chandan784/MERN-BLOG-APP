5db305c4d299d51652e50862f6bd64f9604a63f3
chandan <chandansamantaray784@gmail.com>
2024-03-19 23:40:00 +0530

    hii

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+// app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
 app.use(morgan("dev"));
-app.use('*',(req,res)=>{
-res.sendFile(path.resolve(__dirname,"build",'index.html'))
-})
+// app.use('*',(req,res)=>{
+// res.sendFile(path.resolve(__dirname,"build",'index.html'))
+// })
 
 // app.get("/", (req, res) => {
 //   res.status(200).send({ message: "server ok" });
