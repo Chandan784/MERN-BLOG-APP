d64ff6d75a78a0ec86a696a6bae5a1d73171475b
chandan <chandansamantaray784@gmail.com>
2024-03-25 01:57:51 +0530

    chnages

--- app.js
+++ app.js
@@ -29,10 +29,12 @@ app.use(cors({origin:true,credentials:true}))
 app.use(express.json());
 app.use(express.json());
 app.use(cookieParser())
-app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+// app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+
+app.use(express.static('build'))
 app.use(morgan("dev"));
 app.use('*',(req,res)=>{
-res.sendFile(path.resolve(__dirname,"build",'index.html'))
+res.sendFile(path.join(__dirname,'build/index.html'))
 })
 
 // app.get("/", (req, res) => {
