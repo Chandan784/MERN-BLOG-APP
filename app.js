c0ba05940f7b449fc71e71f4ac2e3fc0b79ebb25
chandan <chandansamantaray784@gmail.com>
2024-04-01 13:40:24 +0530

    final changes

--- app.js
+++ app.js
@@ -25,28 +25,33 @@ const blogRouter = require("./routes/blog")
 const PORT = process.env.PORT 
 const app = express();
 
-app.use(cors({origin:true,credentials:true}))
+app.use(cors())
 app.use(express.json());
-app.use(express.json());
+
 app.use(cookieParser())
-app.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)))
+console.log(__dirname);
+
 
 // app.use(express.static('build'))
 app.use(morgan("dev"));
-app.use('*',(req,res)=>{
-res.sendFile(path.resolve(__dirname,'build','index.html'))
-})
+
 
 // app.get("/", (req, res) => {
 //   res.status(200).send({ message: "server ok" });
 // });
-app.get('/', (req,res)=>{
-        res.status(200).send("sucess")
-})
+// app.get('/', (req,res)=>{
+//         res.status(200).send("sucess")
+// })
 
 app.use('/api/v1/users',userRouter)
 app.use('/api/v1/blogs',blogRouter)
 
+
+
+app.use(express.static(path.resolve(__dirname, "build")))
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'build','index.html'))
+  })
 app.listen(PORT, () => {
   console.log(`server started on ${process.env.DEV_MODE} port no ${PORT}`.bgCyan.white);
 });
