34502d3579980cd7a3eadfbceca70becc6d2d1a5
jayashree patra <jayashreepatra2001@gmail.com>
2024-07-12 16:10:58 +0530

    footer cotent change

--- Blogify/src/Component/Footer/Footer.jsx
+++ Blogify/src/Component/Footer/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
           Blogify blog world
         </h1>
         <p className=" text-sm my-4  text-center lg:text-lg lg:my-4  lg:w-2/4 lg:text-center">
-          Let's explore 1000s+ blogs with Blogify.
+           Blogify is a blog website where you will find great tutorials on web design and development. Here each tutorials is beautifully described step by step with the requried source code.  
         </p>
 
         <div className=" flex gap-2 my-2 lg:flex lg:text-2xl lg:gap-4 lg:my-4">
@@ -29,7 +29,7 @@ function Footer() {
       </div>
       <div className="buttom-footer top-footer h-fit py-4  text-white bg-slate-900 text-center">
         <p className=" text-sm lg:text-lg font-semobold">
-          Copyright @2024 Blogify. Design By Applute
+          Copyright @2024 <span className=" text-blue-600">Blogify</span>. Design By <span className=" text-blue-600">Applute</span>
         </p>
       </div>
     </div>
