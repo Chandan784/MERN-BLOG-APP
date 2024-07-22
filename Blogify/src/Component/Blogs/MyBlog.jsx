89b38f235f483ed7b809c03430c1c02c4a7b02bc
BijayalaxmiAcharya <139479333+BijayalaxmiAcharya@users.noreply.github.com>
2024-07-22 14:47:53 +0530

    git command added and myblogs edit

--- Blogify/src/Component/Blogs/MyBlog.jsx
+++ Blogify/src/Component/Blogs/MyBlog.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { json } from 'express'
+import React, { useEffect } from 'react'
 
 function MyBlog() {
+  let userid =localStorage.getItem("userId")
+  // userid=JSON.parse(userid)
+  console.log(userid)
+ useEffect(()=>{
+  fetch(`/api/v1/blogs/get-blog/${userid}`).then((data)=>{
+console.log(data,"my blogdata")
+  })
+ },[])
+
+
+ 
+
   return (
     <div>MyBlog</div>
+
   )
 }
 
