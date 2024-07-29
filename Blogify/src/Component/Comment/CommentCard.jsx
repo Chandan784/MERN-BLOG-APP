33d734c76ca053b6650e7930f47d5fbebf25a3a8
Manisha956 <mani.sahoo2001@gmail.com>
2024-07-29 12:45:26 +0530

    text added

--- Blogify/src/Component/Comment/CommentCard.jsx
+++ Blogify/src/Component/Comment/CommentCard.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-function CommentCard({data}) {
-  
-
-
+function CommentCard({ data }) {
   return (
     <div className=" w-full lg:w-3/4 flex mt-6 gap-4  ">
       <div className="leftLogo    ">
-        <img className=" h-10   rounded-full "
+        <img
+          className=" h-10   rounded-full "
           src="https://th.bing.com/th/id/OIP.f3TNr7NBbIf89gXmb0wS_QAAAA?w=214&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
           alt=""
         />
@@ -17,12 +15,9 @@ function CommentCard({data}) {
           <h1 className=" text-lg font-medium ">Manisha Sahoo</h1>
           <p>1 day ago</p>
         </div>
-        <p className=" text-normal " >
-          {data}
-        
-        </p>
+        <p className=" text-normal ">{data}</p>
         <div>
-          <h2 className=" font-medium">Reply</h2>
+          <h2 className=" font-medium hover:text-cyan-800">Reply</h2>
         </div>
       </div>
     </div>
